baddcb1e19b08342041dccad43b18a1e04cc528c
Brian Chen <brianchen0810@hotmail.com>
2020-08-13 19:24:29 -0400

    Add initial implementation of retrieving classes

--- app/src/main/java/com/robolancers/dragonclass/MainActivity.java
+++ app/src/main/java/com/robolancers/dragonclass/MainActivity.java
@@ -3,12 +3,41 @@ package com.robolancers.dragonclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.robolancers.dragonclass.utilities.RetrievalCallback;
+import com.robolancers.dragonclass.utilities.RetrieveClasses;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements RetrievalCallback {
+    TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testTextView = findViewById(R.id.test);
+
+        RetrieveClasses retrieveClasses = new RetrieveClasses();
+        retrieveClasses.retrieveClasses(this);
+    }
+
+    @Override
+    public void onComplete(final String result) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                testTextView.setText(result);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/robolancers/dragonclass/utilities/RetrievalCallback.java
@@ -0,0 +1,5 @@
+package com.robolancers.dragonclass.utilities;
+
+public interface RetrievalCallback {
+    void onComplete(String result);
+}
