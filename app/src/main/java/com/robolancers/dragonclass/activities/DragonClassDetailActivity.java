5f0fe14549ee16fc90910d3ae40ec8880fa82a7b
Brian Chen <brianchen0810@hotmail.com>
2020-08-15 18:11:52 -0400

    Show dependencies using RecyclerView

--- app/src/main/java/com/robolancers/dragonclass/activities/DragonClassDetailActivity.java
+++ app/src/main/java/com/robolancers/dragonclass/activities/DragonClassDetailActivity.java
@@ -1,18 +1,24 @@
 package com.robolancers.dragonclass.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.robolancers.dragonclass.R;
+import com.robolancers.dragonclass.adapters.DependencyAdapter;
 import com.robolancers.dragonclass.room.entities.DragonClass;
 import com.robolancers.dragonclass.utilities.Utility;
 
 import java.util.Objects;
 
 public class DragonClassDetailActivity extends AppCompatActivity {
-    private TextView courseID, courseDescription, courseDependencies;
+    private TextView courseID, courseDescription;
+
+    private RecyclerView courseDependencies;
+    private DependencyAdapter dependencyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +27,20 @@ public class DragonClassDetailActivity extends AppCompatActivity {
 
         courseID = findViewById(R.id.course_id);
         courseDescription = findViewById(R.id.course_description);
+
         courseDependencies = findViewById(R.id.course_dependencies);
+        dependencyAdapter = new DependencyAdapter(this);
+
+        courseDependencies.setAdapter(dependencyAdapter);
+        courseDependencies.setLayoutManager(new LinearLayoutManager(this));
 
         DragonClass dragonClass = getIntent().getParcelableExtra("DragonClass");
 
         if (dragonClass != null) {
             courseID.setText(dragonClass.getCourseID());
             courseDescription.setText(dragonClass.getCourseDescription());
-            courseDependencies.setText("Dependencies: " + Objects.requireNonNull(Utility.getInstance().dependencies.get(dragonClass.getCourseID())).toString());
+
+            dependencyAdapter.setCourseIDs(Utility.getInstance().dependencies.get(dragonClass.getCourseID()));
         }
     }
 }
