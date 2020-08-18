5237e630ca5e8952a1855f13aaceb59fa9df25cc
Brian Chen <brianchen0810@hotmail.com>
2020-08-18 00:29:18 -0400

    Remote timeout and maxBodySize to JSoup request

--- app/src/main/java/com/robolancers/dragonclass/activities/DragonMajorListActivity.java
+++ app/src/main/java/com/robolancers/dragonclass/activities/DragonMajorListActivity.java
@@ -5,27 +5,27 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import com.robolancers.dragonclass.MainActivity;
 import com.robolancers.dragonclass.R;
 import com.robolancers.dragonclass.adapters.DragonMajorAdapter;
-import com.robolancers.dragonclass.room.DragonClassDatabase;
-import com.robolancers.dragonclass.room.entities.DragonClass;
 import com.robolancers.dragonclass.utilities.AppExecutors;
-import com.robolancers.dragonclass.utilities.Utility;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DragonMajorListActivity extends AppCompatActivity {
     private RecyclerView dragonMajorRecyclerView;
@@ -42,10 +42,13 @@ public class DragonMajorListActivity extends AppCompatActivity {
 
         AppExecutors.getInstance().diskIO().execute(() -> {
             try {
-                Document document = Jsoup.connect("http://catalog.drexel.edu/coursedescriptions/quarter/undergrad/").get();
+                Document document = Jsoup.connect("http://catalog.drexel.edu/coursedescriptions/quarter/undergrad/")
+                        .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
+                        .maxBodySize(0)
+                        .timeout(0)
+                        .get();
 
-                Element column1 = document.getElementById("listCol1");
-                Elements links = column1.select(":root > div > a");
+                Elements links = document.getElementsByAttributeValueStarting("href", "/coursedescriptions/");
 
                 for (Element link : links) {
                     majors.add(link.text());
