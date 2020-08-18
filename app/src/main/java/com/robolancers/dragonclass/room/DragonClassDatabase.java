c9b6f049309a79b346225089f8bd30fcb5260976
Brian Chen <brianchen0810@hotmail.com>
2020-08-18 02:07:08 -0400

    Add DragonMajor to entities in DragonClassDatabase annotation

--- app/src/main/java/com/robolancers/dragonclass/room/DragonClassDatabase.java
+++ app/src/main/java/com/robolancers/dragonclass/room/DragonClassDatabase.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import com.robolancers.dragonclass.room.daos.DragonClassDao;
 import com.robolancers.dragonclass.room.daos.DragonMajorDao;
 import com.robolancers.dragonclass.room.entities.DragonClass;
+import com.robolancers.dragonclass.room.entities.DragonMajor;
+
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {DragonClass.class}, version = 1, exportSchema = false)
+@Database(entities = {DragonClass.class, DragonMajor.class}, version = 2, exportSchema = false)
 public abstract class DragonClassDatabase extends RoomDatabase {
     public abstract DragonClassDao dragonClassDao();
     public abstract DragonMajorDao dragonMajorDao();
