abe6f4adad271e72ce4c221445d2da248b4722c7
Brian Chen <brianchen0810@hotmail.com>
2020-08-19 01:08:16 -0400

    Get the right classes from Room

--- app/src/main/java/com/robolancers/dragonclass/room/daos/DragonClassDao.java
+++ app/src/main/java/com/robolancers/dragonclass/room/daos/DragonClassDao.java
@@ -1,6 +1,8 @@
 package com.robolancers.dragonclass.room.daos;
 
 import com.robolancers.dragonclass.room.entities.DragonClass;
+import com.robolancers.dragonclass.room.entities.DragonMajor;
+import com.robolancers.dragonclass.room.relationships.DragonMajorWithDragonClasses;
 
 import java.util.List;
 
@@ -9,6 +11,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Transaction;
 
 @Dao
 public interface DragonClassDao {
@@ -20,4 +23,7 @@ public interface DragonClassDao {
 
     @Query("SELECT * from class_table ORDER BY course_id ASC")
     LiveData<List<DragonClass>> getAllClasses();
+
+    @Query("SELECT * FROM class_table WHERE parent_major_name = :majorName")
+    LiveData<List<DragonClass>> getClassesWithMajor(String majorName);
 }
