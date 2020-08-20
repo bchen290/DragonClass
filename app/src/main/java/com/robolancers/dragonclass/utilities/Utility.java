1bffd5a0af9d1b0609637568605bcc6036032103
Brian Chen <brianchen0810@hotmail.com>
2020-08-20 16:31:39 -0400

    Only download if not downloaded before

--- app/src/main/java/com/robolancers/dragonclass/utilities/Utility.java
+++ app/src/main/java/com/robolancers/dragonclass/utilities/Utility.java
@@ -1,6 +1,8 @@
 package com.robolancers.dragonclass.utilities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -8,6 +10,7 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.robolancers.dragonclass.R;
 import com.robolancers.dragonclass.room.DragonClassDatabase;
 import com.robolancers.dragonclass.room.entities.DragonClass;
 import com.robolancers.dragonclass.room.entities.DragonMajor;
@@ -31,95 +34,101 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class Utility {
     public static void downloadMajorsAndClasses(Activity activity, TextView progressText, RecyclerView dragonMajorRecyclerView, LinearLayout progressHolder) {
-        AppExecutors.getInstance().diskIO().execute(() -> {
-            try {
-                activity.runOnUiThread(() -> progressText.setText("Downloading courses"));
+        SharedPreferences sharedPreferences = activity.getSharedPreferences(activity.getString(R.string.app_name), Context.MODE_PRIVATE);
 
-                Connection.Response response = Jsoup.connect("http://catalog.drexel.edu/coursedescriptions/quarter/undergrad/")
-                        .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36")
-                        .referrer("http://www.google.com")
-                        .maxBodySize(0)
-                        .timeout(0)
-                        .followRedirects(true)
-                        .execute();
+        if (!sharedPreferences.getBoolean("hasDownloaded", false)) {
+            AppExecutors.getInstance().diskIO().execute(() -> {
+                try {
+                    activity.runOnUiThread(() -> progressText.setText("Downloading courses"));
 
-                if (response.statusCode() < 400) {
-                    Document document = response.parse();
-                    Elements links = document.getElementsByAttributeValueStarting("href", "/coursedescriptions/quarter/undergrad/");
-                    HashMap<String, List<String>> dependencies = new HashMap<>();
+                    Connection.Response response = Jsoup.connect("http://catalog.drexel.edu/coursedescriptions/quarter/undergrad/")
+                            .userAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36")
+                            .referrer("http://www.google.com")
+                            .maxBodySize(0)
+                            .timeout(0)
+                            .followRedirects(true)
+                            .execute();
 
-                    for (Element link : links) {
-                        DragonClassDatabase.getDatabase(activity).dragonMajorDao().insert(new DragonMajor(link.text()));
-                        activity.runOnUiThread(() -> progressText.setText("Downloading " + link.text() + "'s classes"));
+                    if (response.statusCode() < 400) {
+                        Document document = response.parse();
+                        Elements links = document.getElementsByAttributeValueStarting("href", "/coursedescriptions/quarter/undergrad/");
+                        HashMap<String, List<String>> dependencies = new HashMap<>();
 
-                        document = Jsoup.connect("http://catalog.drexel.edu" + link.attr("href")).get();
-                        Elements courseBlocks = document.getElementsByClass("courseblock");
+                        for (Element link : links) {
+                            DragonClassDatabase.getDatabase(activity).dragonMajorDao().insert(new DragonMajor(link.text()));
+                            activity.runOnUiThread(() -> progressText.setText("Downloading " + link.text() + "'s classes"));
 
-                        for (Element course : courseBlocks) {
-                            Element courseBlockTitleElement = course.getElementsByClass("courseblocktitle").first();
-                            Element courseBlockDescriptionElement = course.getElementsByClass("courseblockdesc").first();
+                            document = Jsoup.connect("http://catalog.drexel.edu" + link.attr("href")).get();
+                            Elements courseBlocks = document.getElementsByClass("courseblock");
 
-                            String[] courseBlockTitleSplit = courseBlockTitleElement.text().split(" ");
+                            for (Element course : courseBlocks) {
+                                Element courseBlockTitleElement = course.getElementsByClass("courseblocktitle").first();
+                                Element courseBlockDescriptionElement = course.getElementsByClass("courseblockdesc").first();
 
-                            String title = Arrays.stream(courseBlockTitleSplit, 2, courseBlockTitleSplit.length - 2).reduce("", (subtotal, element) -> subtotal + " " + element);
+                                String[] courseBlockTitleSplit = courseBlockTitleElement.text().split(" ");
 
-                            String courseText = course.text();
-                            String[] courseTextSplit = courseText.split("Prerequisites:");
+                                String title = Arrays.stream(courseBlockTitleSplit, 2, courseBlockTitleSplit.length - 2).reduce("", (subtotal, element) -> subtotal + " " + element);
 
-                            String prerequisites = courseTextSplit.length == 2 ? courseTextSplit[1] : "N/A";
-                            String[] prerequisitesSplit = prerequisites.split("and|or");
+                                String courseText = course.text();
+                                String[] courseTextSplit = courseText.split("Prerequisites:");
 
-                            DragonClass dragonClass = new DragonClass(
-                                    courseBlockTitleSplit[0] + courseBlockTitleSplit[1],
-                                    title,
-                                    courseBlockDescriptionElement.text(),
-                                    prerequisites,
-                                    link.text(),
-                                    ""
-                            );
+                                String prerequisites = courseTextSplit.length == 2 ? courseTextSplit[1] : "N/A";
+                                String[] prerequisitesSplit = prerequisites.split("and|or");
 
-                            DragonClassDatabase.getDatabase(activity).dragonClassDao().insert(dragonClass);
-                            dependencies.put(dragonClass.getCourseID(), new ArrayList<>());
+                                DragonClass dragonClass = new DragonClass(
+                                        courseBlockTitleSplit[0] + courseBlockTitleSplit[1],
+                                        title,
+                                        courseBlockDescriptionElement.text(),
+                                        prerequisites,
+                                        link.text(),
+                                        ""
+                                );
 
-                            for (String prerequisite : prerequisitesSplit) {
-                                String[] prerequisiteSplit = prerequisite.trim().split(" ");
+                                DragonClassDatabase.getDatabase(activity).dragonClassDao().insert(dragonClass);
+                                dependencies.put(dragonClass.getCourseID(), new ArrayList<>());
 
-                                if (!prerequisite.equals("N/A") && !prerequisite.trim().isEmpty()) {
-                                    String prerequisiteCourseID = (prerequisiteSplit[0].replaceAll("\\(", "") + prerequisiteSplit[1]);
+                                for (String prerequisite : prerequisitesSplit) {
+                                    String[] prerequisiteSplit = prerequisite.trim().split(" ");
 
-                                    if (dependencies.containsKey(prerequisiteCourseID)) {
-                                        if (!Objects.requireNonNull(dependencies.getOrDefault(prerequisiteCourseID, new ArrayList<>())).contains(dragonClass.getCourseID().replace("\\s", ""))) {
-                                            dependencies.computeIfPresent(prerequisiteCourseID, (k, v) -> {
-                                                v.add(dragonClass.getCourseID());
-                                                return v;
-                                            });
+                                    if (!prerequisite.equals("N/A") && !prerequisite.trim().isEmpty()) {
+                                        String prerequisiteCourseID = (prerequisiteSplit[0].replaceAll("\\(", "") + prerequisiteSplit[1]);
+
+                                        if (dependencies.containsKey(prerequisiteCourseID)) {
+                                            if (!Objects.requireNonNull(dependencies.getOrDefault(prerequisiteCourseID, new ArrayList<>())).contains(dragonClass.getCourseID().replace("\\s", ""))) {
+                                                dependencies.computeIfPresent(prerequisiteCourseID, (k, v) -> {
+                                                    v.add(dragonClass.getCourseID());
+                                                    return v;
+                                                });
+                                            }
+                                        } else {
+                                            dependencies.put(prerequisiteCourseID, new ArrayList<>(Collections.singletonList(dragonClass.getCourseID().replace("\\s", ""))));
                                         }
-                                    } else {
-                                        dependencies.put(prerequisiteCourseID, new ArrayList<>(Collections.singletonList(dragonClass.getCourseID().replace("\\s", ""))));
                                     }
                                 }
                             }
                         }
+
+                        Gson gson = new Gson();
+
+                        for (Map.Entry<String, List<String>> dependency : dependencies.entrySet()) {
+                            DragonClass dragonClass = DragonClassDatabase.getDatabase(activity).dragonClassDao().getClassByCourseID(dependency.getKey()).getValue().get(0);
+                            dragonClass.setDependencies(gson.toJson(dependency.getValue()));
+                            DragonClassDatabase.getDatabase(activity).dragonClassDao().insert(dragonClass);
+                        }
+
+                        activity.runOnUiThread(() -> {
+                            dragonMajorRecyclerView.setVisibility(View.VISIBLE);
+                            progressHolder.setVisibility(View.GONE);
+
+                            sharedPreferences.edit().putBoolean("hasDownloaded", true).apply();
+                        });
+                    } else {
+                        progressText.setText(response.statusMessage());
                     }
-
-                    Gson gson = new Gson();
-
-                    for (Map.Entry<String, List<String>> dependency : dependencies.entrySet()) {
-                        DragonClass dragonClass = DragonClassDatabase.getDatabase(activity).dragonClassDao().getClassByCourseID(dependency.getKey()).getValue().get(0);
-                        dragonClass.setDependencies(gson.toJson(dependency.getValue()));
-                        DragonClassDatabase.getDatabase(activity).dragonClassDao().insert(dragonClass);
-                    }
-
-                    activity.runOnUiThread(() -> {
-                        dragonMajorRecyclerView.setVisibility(View.VISIBLE);
-                        progressHolder.setVisibility(View.GONE);
-                    });
-                } else {
-                    progressText.setText(response.statusMessage());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+            });
+        }
     }
 }
